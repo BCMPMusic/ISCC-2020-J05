df9dc45d5b44fc3664e7181e093a6c6c219204e5
BuildTools <unconfigured@null.spigotmc.org>
2020-06-29 13:06:53 +0200

    Opérations simples exo 3
    Quelques ajouts d'opérations

---
+++ EX_03/t-shirt.php
@@ -0,0 +1,16 @@
+<?php
+$nom_produit = 'T-shirt simple';
+$couleur = 'Blanc';
+$prix = '10.50';
+$disponible = 'true';
+$quantité = '10';
+$a = '31.50';
+$b = '105';
+
+echo "<h3> Phrase 1     Le nom du produit est $nom_produit <br/>";
+echo "<h3> Phrase 2     Il reste $quantité produit en stock <br/>";
+echo "<h3> Phrase 3     Le produit $nom_produit est de couleur $couleur <br/>";
+
+echo "<h4> Phrase 1     Acheter 3 produits couterait $prix + $prix + $prix <br/>";
+echo "<h4> Phrase 2     Acheter la totalité des produits disponibles coûterait $prix * $quantité <br/>";
+echo "<h4> Phrase 3     Si 3 produits sont vendus <br/>";
